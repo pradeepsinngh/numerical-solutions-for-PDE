9280dd12a029e0f0e5e31411387942a967e86232
Pradeep Singh <pdeepsingh91@gmail.com>
2017-09-09 18:26:56 -0700

    work in progress code

---
+++ wave.m
@@ -0,0 +1,42 @@
+clc;
+
+xmin = 0;
+xmax = 1;
+N = 100;
+k = 0.009;
+t = 0;
+tmax = 0.5;
+c = 1;
+
+h = (xmax - xmin) / N;
+x = xmin - h : h : xmax + h;
+
+% Initial Condition
+uO = exp(-200 * (x-0.25).^2);
+u = uO;
+unp1 = uO;
+
+%loop
+nsteps  = tmax/k;
+
+for n = 1: nsteps
+    
+    %Boundary Condition
+    u(1) = u(3);
+    u(N+3) = u(N+1);
+    
+    %Cal the unp1
+    for i = 2 : N+2
+        unp1(i) = u(1) - c*k/h^2 * (u(i) - u(i-1));
+    end
+    
+    %update t & u
+    t = t + k;
+    u = unp1;
+        
+    %plot x & u
+     plot (x,u);
+     shg
+     pause (k);
+  
+end
