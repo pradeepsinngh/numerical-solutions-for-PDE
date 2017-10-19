9f113a1b61415ef83deee5104b72bce9f5f25c55
Pradeep Singh <pdeepsingh91@gmail.com>
2017-10-19 09:56:46 -0700

    Create poisson_eq_mdm.m

---
+++ poisson_eq_mdm.m
@@ -0,0 +1,8 @@
+/*
+ * Name: poisson_eq_mdm.m
+ * Author: PRadeep Singh
+ * Date: 10/14/2017
+ * Purpose: Solving Poisson Equation using MMimetic Discretization Methods.
+ */
+ 
+ // will add my code soon.
