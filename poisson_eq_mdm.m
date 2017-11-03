92cf76ec65403d72a2c660d8e3e1eee4b31286c6
Pradeep Singh <pdeepsingh91@gmail.com>
2017-11-03 11:31:27 -0700

    Update poisson_eq_mdm.m

--- poisson_eq_mdm.m
+++ poisson_eq_mdm.m
@@ -1,4 +1,4 @@
-%% 2D Poisson Equation
+% 2D Poisson Equation
 
 % Author: Pradeep Singh
 % Email: psingh2@sdsu.edu
@@ -6,13 +6,13 @@
 % Numerical Solution for 2D Poisson Equation 
 % using Mimetic Discretization Method (MDM)
 
-%% Getting Started
+% Getting Started
 
 clear; clc  % clear workspace
 
 addpath('../Matlab/mole-master/mole_MATLAB') % path of mole library
 
-%% Inputs
+% Inputs
 
 m = 10;  % grid points
 n = 10;  % grid points
@@ -24,7 +24,7 @@ dx = (b-a)/m;  % Step length along the x-axis
 dy = (d-c)/n;  % Step length along the y-axis
 k = 2;
 
-%% Laplacian Mattix (L)
+% Laplacian Mattix (L)
 
 % Initalize Matrices, with zeros and identity matrix
 
@@ -36,7 +36,7 @@ Q = zeros(144,144) + eye(144,144);
 L = lap2D(k, m, dx, n, dy);
 L = L + Q;
 
-%% Z Matrix
+% Z Matrix
 
 % z matrix to compute f matrix
 
@@ -46,14 +46,14 @@ z = zeros(n,n);      % square matrix to compute f matrix
 x = linspace(0,1,12);  % x axis with n grid points
 y = linspace(0,1,12);  % y axis with m gridd points
 
-%% Boundary Condition
+% Boundary Condition
 
 z(1,:) = 0;   % zero on 1st row
 z(n,:) = 0;   % zero on last row
 z(:,1) = 0;   % zero on 1st col
 z(:,n) = 0;   % zero on last col
 
-%% Calculate Z matrix 
+% Calculate Z matrix 
 
 % for loop from 2nd row and 2nd col till (h-1) row and 
 % column to compute z(i,j)
@@ -64,7 +64,7 @@ for i = 2:1:n-1             % row
     end
 end
 
-%% Calculate f matrix
+% Calculate f matrix
 
 % For loop to arrange all elements of z matrix in 
 % column vector format
@@ -77,7 +77,7 @@ for i = 1:1:n             % row
    end
 end
 
-%% U matrix (Numerical Solution)
+% U matrix (Numerical Solution)
 
 % Caculate "U" (Numerical solution) uisng "\"(backslash) operator.
 
@@ -88,7 +88,7 @@ U = L\f;
 
 B = reshape(U,[n,n]);
 
-%% Plots
+% Plots
 % Plot surface image of the B matrix
 
 surf(x,y,B)
@@ -98,7 +98,7 @@ ylabel('y axis')                                     % y-axis label
 zlabel('U(x,y)')                                     % z-axis label
 grid on
 
-%% Conclusion
+% Conclusion
 
 % We plotted Numerical Solution for 2D Poisson Equation using 
 % Mimetic Discretization Method (MDM). 
